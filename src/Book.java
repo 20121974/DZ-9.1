b050ff30de73b8df7dbb5b0263c22337cb291b18
20121974 <nfi46102@mail.ru>
2022-09-02 09:25:56 +0300

    5 commit

--- src/Book.java
+++ src/Book.java
@@ -1,21 +1,28 @@
 public class Book {
     private final String titulName;
     private int datePublish;
-
+    private String authorName;
 
     public Book(String titulName, int datePublish ) {
         this.titulName = titulName;
         this.datePublish = datePublish;
+        this.authorName = authorName;
     }
 
     public String getTitulName() {
+
         return titulName;
     }
+    public String getAuthorName() {
 
+        return getAuthorName();
+    }
     public int getDatePublish() {
+
         return datePublish;
     }
     public void setDatePublish(int datePublish) {
+
         this.datePublish = datePublish;
     }
 
